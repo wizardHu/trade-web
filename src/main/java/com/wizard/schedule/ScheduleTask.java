37b26321e0a5d98ad81def607283a3de1b1c8671
hulujie <741802012@qq.com>
2020-08-29 22:14:42 +0800

    Scheduled

--- src/main/java/com/wizard/schedule/ScheduleTask.java
+++ src/main/java/com/wizard/schedule/ScheduleTask.java
@@ -2,6 +2,7 @@ package com.wizard.schedule;
 
 import com.wizard.service.ManualTradeService;
 import com.wizard.service.StatisticService;
+import com.wizard.service.StrategyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,9 @@ public class ScheduleTask {
     @Resource
     private ManualTradeService manualTradeService;
 
+    @Resource
+    private StrategyService strategyService;
+
 
     @Scheduled(cron = "0 0 22 * * ?")
     private void statisticBalance() {
@@ -31,6 +35,13 @@ public class ScheduleTask {
         log.info("begin checkManualTrade");
         manualTradeService.checkManualTrade();
         log.info("end checkManualTrade");
+    }
+
+    @Scheduled(cron = "0 */10 * * * ?")
+    private void macdStrategy() {
+        log.info("begin macdStrategy");
+        strategyService.macdStrategy();
+        log.info("end macdStrategy");
     }
 
 }
